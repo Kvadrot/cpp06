363d7b7e911420ce5f823e29e381b742bc04bcc1
Kvadrot <send.me.your.maid@gmail.com>
2025-09-11 13:26:24 +0200

    printing for all types, TODO recheck the floatPrinting

--- ex00/ScalarConvertor.cpp
+++ ex00/ScalarConvertor.cpp
@@ -6,7 +6,7 @@
 /*   By: ufo <ufo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 11:19:27 by ufo               #+#    #+#             */
-/*   Updated: 2025/09/11 12:43:48 by ufo              ###   ########.fr       */
+/*   Updated: 2025/09/11 13:23:30 by ufo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,65 @@ void printInputErr(const std::string& text) {
     std::cout << text << " is invalid input" << std::endl;
 }
 
+
  void printPseudo(const std::string& text) {
     std::cout << "char: impossible\nint: impossible\n";
     if (text == "nanf" || text == "+inff" || text == "-inff") {
         std::cout << "float: "  << text << "\n";
-        std::cout << "double: " << (text == "nanf" ? "nan" : (text[0]=='-'?"-inf":"+inf")) << "\n";
+        std::cout << "double: " << (text == "nanf" ? "nan" : (text[0]== '-' ? "-inf" : "+inf")) << "\n";
     } else {
-        std::cout << "float: "  << (text == "nan" ? "nanf" : (text[0]=='-'?"-inff":"+inff")) << "\n";
+        std::cout << "float: "  << (text == "nan" ? "nanf" : (text[0]== '-' ? "-inff" : "+inff")) << "\n";
         std::cout << "double: " << text << "\n";
     }
     return;
  }
+ 
+
+ void print_char_from_double(const double& numD) {
+    int intedNumD = 0;
+    if (numD < 0.0 || numD > 127) {
+        std::cout << "char: impossible" << std::endl;
+        return;
+    }
+    intedNumD = static_cast<int>(numD);
+	if (intedNumD < 32 || intedNumD == 127) { 
+        std::cout << "char: Non displayable\n";
+        return;
+    }
+    std::cout << "char: " << static_cast<char>(intedNumD) << std::endl;
+ }
+ 
+
+ void print_int_from_double(const double& numD) {
+    int intedNumD = 0;
+    if (numD != numD || numD < MIN_INT || numD > MAX_INT) {
+        std::cout << "int: impossible" << std::endl;
+        return;
+    }
+    intedNumD = static_cast<int>(numD);
+    std::cout << "int: " << intedNumD << std::endl;
+ }
+ 
+
+  void print_float_from_double(const double& numD) {
+    int floatedNumD = 0.0f;
+    if (numD < MIN_FLOAT || numD > MAX_FLOAT) {
+        std::cout << "float: impossible" << std::endl;
+        return;
+    }
+    floatedNumD = static_cast<float>(numD);
+    std::cout << "float: " << floatedNumD << std::endl;
+ }
+ 
+
+   void print_double(const double& numD) {
+    if (numD < MIN_DOUBLE || numD > MAX_DOUBLE) {
+        std::cout << "double: impossible" << std::endl;
+        return;
+    }
+    std::cout << "double: " << numD << std::endl;
+ }
+ 
 
 //Casting funcs
 //
@@ -63,9 +111,9 @@ double castCharToDouble(const std::string& text) {
 }
 
 bool castStrToIntableDouble(const std::string text, double& inoutWD) {
-    long long num = 0;
+    long num = 0;
     char *end = 0;
-    num = std::strtoll(text.c_str(), &end, 10);
+    num = std::strtol(text.c_str(), &end, 10);
 
     if (errno == ERANGE || end == text.c_str() || *end != '\0')
         return false;
@@ -74,16 +122,15 @@ bool castStrToIntableDouble(const std::string text, double& inoutWD) {
     return true;
 }
 
-bool castStrToFloatableDouble(const std::string text, double& inoutWD) {
-    double numD = 0;
-    char *end = 0;
-    numD = std::strtod(text.c_str(), &end);
+bool castStrToFloatableDouble(const std::string text, double &out) {
+	std::string core = text.substr(0, text.size()-1);
+	char *end = 0;
+	double res = std::strtod(core.c_str(), &end);
+	if (errno == ERANGE || end == core.c_str() || *end != '\0')
+		return false;
+	out = res; // pivot is double
 
-    if (errno == ERANGE || end == text.c_str() || *end != '\0')
-        return false;
-
-    inoutWD = static_cast<double>(numD);
-    return true;
+	return true;
 }
 
 bool castStrToDouble(const std::string text, double& inoutWD) {
@@ -125,5 +172,8 @@ void ScalarConvertor:: convert(const std::string& text) {
         default: //typeInvalid:
             break;
     }
-    std::cout << wildCard << " ====== " << std::endl;
+    print_char_from_double(wildCard);
+	print_int_from_double(wildCard);
+	print_float_from_double(wildCard);
+	print_double(wildCard);
 }
